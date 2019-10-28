eb8187f3c364c05d885e0d701acc03de8b1183f1
ben black <weepingwillowben@gmail.com>
2019-10-28 14:04:34 -0400

    removed weighted sum from agentdecisionmaker

--- scripts/agent_ai.py
+++ scripts/agent_ai.py
@@ -9,16 +9,11 @@ class AgentDecisionMaker:
         #self.map = dict()
         self.x=0
         self.y=0
-        self.prevx = 0
-        self.prevy = 0
 
     def on_sight(self,pointcloud,coords_seen):
         pointcloud += coords_seen
         xweight = random.random()*2-1
         yweight = random.random()*2-1
-
-        #xweight += directed_sqr(self.x - self.prevx)
-        #yweight += directed_sqr(self.y - self.prevy)
 
         for obj_type,point_offset in pointcloud:
             xoff = point_offset[0] #- self.x
@@ -41,8 +36,5 @@ class AgentDecisionMaker:
 
 
     def on_move(self,delta_pos,delta_reward):
-        DECAY_VAL = 0.9
-        self.prevx = self.prevx*DECAY_VAL+self.x*(1-DECAY_VAL)
-        self.prevy = self.prevy*DECAY_VAL+self.y*(1-DECAY_VAL)
         self.x += delta_pos[0]
         self.y += delta_pos[1]
