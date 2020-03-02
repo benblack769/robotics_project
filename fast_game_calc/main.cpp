1bd2ae74ce2738a9921cbb3791839b965b5f7ecb
weepingwillowben <weepingwillowben@gmail.com>
2020-03-02 16:34:59 -0500

    fixed plotting x axis

--- fast_game_calc/main.cpp
+++ fast_game_calc/main.cpp
@@ -748,15 +748,15 @@ void compete_paths(ConstantGameInfo & gi,
         //evaluate theif rewards
 
         const size_t PAR_RESPONSES = numHWThreads;
-        const size_t NUM_RESPONSE_PATHS = 1000/PAR_RESPONSES;
-        const size_t NUM_RESPONSE_ITERS = 10*NUM_RESPONSE_PATHS;
+        const size_t NUM_RESPONSE_PATHS = 60000/PAR_RESPONSES;
+        const size_t NUM_RESPONSE_ITERS = 1;
         PathCollection thief_responses;
         PathRewards thief_response_rews;
         PathCollection theif_start;
         PathRewards theif_rew_start;
         if(iter_n > 200){
-            theif_start = theif_paths;
-            theif_rew_start = thief_rewards;
+            //theif_start = theif_paths;
+            //theif_rew_start = thief_rewards;
         }
         parallel_response_fn(
                     theif_start,
@@ -791,8 +791,8 @@ void compete_paths(ConstantGameInfo & gi,
         PathCollection guard_start;
         PathRewards guard_rew_start;
         if(iter_n > 200){
-            guard_start = guard_paths;
-            guard_rew_start = guard_rewards;
+            //guard_start = guard_paths;
+            //guard_rew_start = guard_rewards;
         }
         parallel_response_fn(
                     guard_start,
--- plot_weights.py
+++ plot_weights.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     data = pandas.read_csv(args.csv_fname)
-    num_updates = 1250
-    xvals = np.arange(len(data.guard_rew)) * num_updates
-    data["x"] = xvals
-    plt.plot('x', 'guard_rew',data=data , marker='o', markerfacecolor='blue', markersize=4, color='skyblue', linewidth=2)
-    plt.plot('x', 'thief_rew',data=data, marker='o', markerfacecolor='red', markersize=4, color='#ff9999', linewidth=2)
-    plt.plot('x', 'guard_report_reward',data=data, marker='o', markerfacecolor='blue', markersize=4, color='blue', linewidth=2)
-    plt.plot('x', 'thief_report_reward',data=data, marker='o', markerfacecolor='red', markersize=4, color='red', linewidth=2)
+    #num_updates = 1250
+    #xvals = np.arange(len(data.guard_rew)) * num_updates
+    #data["x"] = xvals
+    plt.plot('row_idx', 'guard_rew',data=data , marker='o', markerfacecolor='blue', markersize=4, color='skyblue', linewidth=2)
+    plt.plot('row_idx', 'thief_rew',data=data, marker='o', markerfacecolor='red', markersize=4, color='#ff9999', linewidth=2)
+    plt.plot('row_idx', 'guard_report_reward',data=data, marker='o', markerfacecolor='blue', markersize=4, color='blue', linewidth=2)
+    plt.plot('row_idx', 'thief_report_reward',data=data, marker='o', markerfacecolor='red', markersize=4, color='red', linewidth=2)
     plt.legend()
 
     #plt.show()
